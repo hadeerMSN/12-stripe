24346dea5e8564f29133faba0bd1d41250d3f362
hadeerMSN <HadeerM@trustsecurenow.com>
2022-09-28 15:56:33 +0200

    stripe

---
+++ src/Submenu.js
@@ -0,0 +1,24 @@
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
+
+const Submenu = () => {
+  const { isSubmenuOpen, location } = useGlobalContext();
+  const container = useRef(null);
+
+  useEffect(() => {
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+  }, [location]);
+  return (
+    <aside
+      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
+      ref={container}
+    >
+      hello
+    </aside>
+  );
+};
+
+export default Submenu;
